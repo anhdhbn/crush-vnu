7cbf06dd513919c77a7d427d2bef3a3a46206eae
Anh DH <anhdhbn@gmail.com>
2019-10-11 20:02:27 +0700

    optimize code

--- crawl_data/crawler/images/photo_of.py
+++ crawl_data/crawler/images/photo_of.py
@@ -1,16 +1,18 @@
-from crawler.utilities.utilities import CommonUtilities
+from crawler.images.common_image import CommonImage
 from crawler.init.constants import IMAGE
 import sys
 
-class PhotoOf(CommonUtilities):
+class PhotoOf(CommonImage):
     def execute_script(self, link_user: str):
         self.driver.get(self.get_link_photos_of(link_user))
         self.scroll()
         elements = self.safe_get_elements_by_xpath(IMAGE['XPATH_EACH_IMAGE'])
         images = self.get_link_images(elements)
         link_images = self.get_download_images_url(images)
-        link_images = [link for link in link_images if link != "None"]
-        self.image_downloader(link_images, self.create_original_link(link_user))
+        link_images = [(link, [box for box in boxs if box != ''])  for link, boxs in link_images if link != "None"]
+        print(link_images)
+        # nay mai se chuyen link download cho queue download
+        # self.image_downloader(link_images, self.create_original_link(link_user))
 
     def get_link_photos_of(self, link_user: str) -> str:
         original_link = self.create_original_link(link_user)
--- crawl_data/crawler/utilities/utilities.py
+++ crawl_data/crawler/utilities/utilities.py
@@ -1,10 +1,10 @@
 from crawler.init.init import InitSelenium
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import os
-import urllib.request
+
 import sys
 
 class CommonUtilities(InitSelenium):
@@ -122,77 +122,3 @@ class CommonUtilities(InitSelenium):
             return self.driver.find_element_by_id(elem_id)
         except NoSuchElementException:
             return None
-
-    def parse_tagbox(self, tag):
-        try:
-            return tag.get_attribute("style")
-        except:
-            return None
-
-    def get_download_images_url(self, img_links):
-        urls = []
-
-        for link in img_links:
-            if link != "None":
-                valid_url_found = False
-                self.driver.get(link)
-
-                try:
-                    while not valid_url_found:
-                        WebDriverWait(self.driver, self.timeout_second).until(EC.presence_of_element_located((By.CLASS_NAME, "spotlight")))
-                        element = self.driver.find_element_by_class_name("spotlight")
-                        img_url = element.get_attribute('src')
-
-                        if img_url.find('.gif') == -1:
-                            valid_url_found = True
-                            tag_elements = self.safe_get_elements_by_css_selector(".fbPhotosPhotoTagboxBase")
-                            if tag_elements is not None:
-                                if len(tag_elements) > 0:
-                                    tag_box = [self.parse_tagbox(tag) for tag in tag_elements]
-                                    tag_box = [tag for tag in tag_box if tag is not None]
-                                    urls.append((img_url, tag_box))
-                except Exception as e:
-                    print(e)
-            else:
-                print(e)
-
-        return urls
-    
-    def create_folder(self, folder):
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-
-    def get_filename_from_link(self, link):
-        return (link.split('.jpg')[0]).split('/')[-1] + '.jpg'
-
-    def download_from_url(self, link, filepath, img_name):
-        print("Downloading ", img_name)
-        try:
-            urllib.request.urlretrieve(link, filepath)
-            return link, filepath, img_name
-        except:
-            return link, filepath, None
-        
-
-    def image_downloader(self, img_links, original_link):
-        folder_name = original_link.split("/")[-1]
-
-        img_names = []
-
-        try:
-            try:
-                folder = os.path.join(os.getcwd(),"data", folder_name)
-                self.create_folder(folder)
-            except Exception:
-                print("Error in changing directory.")
-
-            arr_download = [(link, self.get_filename_from_link(link)) for link in img_links]
-            arr_download = [(link, filename) for (link, filename) in arr_download if (filename != "10354686_10150004552801856_220367501106153455_n.jpg")]
-            arr_download = [(link, os.path.join(folder, filename), filename) for link, filename in arr_download]
-
-            img_names = [self.download_from_url(link,filepath, filename ) for (link, filepath, filename) in arr_download]
-            img_names = [(link, filepath, filename) for (link, filepath, filename) in arr_download if filename is not None]
-        except Exception as e:
-            print("Exception (image_downloader):", e)
-
-        return img_names
