128054a6f39bece03ed366acd21a5eeec6b94deb
Anh DH <anhdhbn@gmail.com>
2019-10-16 20:07:15 +0700

    optimize code (constants.py)

---
+++ crawl-data/crawler/facebook/constants.py
@@ -0,0 +1,5 @@
+IMAGE = {}
+
+IMAGE['CSS_TAB'] = "#pagelet_timeline_medley_photos div[role='tablist'] > a"
+IMAGE['XPATH_EACH_IMAGE'] = "//a[contains(@id, 'pic_')]"
+IMAGE['CSS_TAGS_BOX'] = ".fbPhotosPhotoTagboxBase"
