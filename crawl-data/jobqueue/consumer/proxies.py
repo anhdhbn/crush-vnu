01642038f8b8f88c88a36f07b8d576739f6196b5
Anh DH <anhdhbn@gmail.com>
2019-10-20 17:35:40 +0700

    add proxy sites

--- crawl-data/jobqueue/consumer/proxies.py
+++ crawl-data/jobqueue/consumer/proxies.py
@@ -1,6 +1,5 @@
 def check_fresh(proxy):
     from crawler.proxies.check_fresh import CheckFresh
     checker = CheckFresh(proxy=proxy)
-    if checker.execute_script() is not None:
-        return checker.execute_script()
+    return checker.execute_script()
     
