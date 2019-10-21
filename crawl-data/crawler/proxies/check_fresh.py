f205e83c85f2f1b0c283e0c4e5bfac388523a353
AnhDH <anhdhbn@gmail.com>
2019-10-22 01:00:12 +0700

    optimize code

--- crawl-data/crawler/proxies/check_fresh.py
+++ crawl-data/crawler/proxies/check_fresh.py
@@ -1,14 +1,15 @@
 from crawler.init.initchilkat import InitChilkat
+import chilkat
 
 class CheckFresh(InitChilkat):
     def execute_script(self):
-        # return self.http.quickGetStr(f"http://ip-api.com/json/{self.ip}")
-        task = self.http.QuickGetStrAsync(f"http://ip-api.com/json/")
+        # return self.http.quickGetStr(f"http://ip-api.com/json/")
+        task = self.http.QuickGetObjAsync(f"http://ip-api.com/json/")
         task.Run()
         status = task.Wait(self.timeout * 1000)
         wasCanceled = task.Cancel()
         if(wasCanceled):
             return "Timeout"
         else:
-            result = task.getResultString()
+            result = self.load_response(task)
             return result
